a966ff919ebae938965d1b99defe5f45f16a3c1b
olbailey <olbailey@outlook.com>
2026-07-29 10:51:13 +0100

    added dataset download script

---
+++ scripts/download_data.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+
+import kagglehub
+
+DESTINATION = "data/raw"
+
+os.makedirs(DESTINATION, exist_ok=True)
+
+# Download latest version
+path = kagglehub.dataset_download("samdazel/automated-cardiac-diagnosis-challenge-miccai17")
+cache_path = os.path.join(path, "database/")
+
+print("Path to dataset files:", cache_path)
+
+
+for item in os.listdir(cache_path):
+    source = os.path.join(cache_path, item)
+    dest = os.path.join(DESTINATION, item)
+    shutil.move(source, dest)
+
+shutil.rmtree(path)
+
+print("Dataset moved to:", DESTINATION)
