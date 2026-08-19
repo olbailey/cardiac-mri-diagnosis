8e3946ac7c81c6e87d587316916cfeb5575decfc
olbailey <olbailey@outlook.com>
2026-08-19 23:08:31 +0100

    Improved Dataset to correct pass image and mask to Monai transform, also adjusted transforms to use Monai transforms

--- src/dataset/mri_dataset.py
+++ src/dataset/mri_dataset.py
@@ -5,8 +5,11 @@ import nibabel as nib
 
 import torch
 from torch.utils.data import Dataset, Subset, DataLoader
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+import monai
+from monai.transforms import (
+    Compose, LoadImaged, EnsureChannelFirstd, EnsureTyped, ResizeWithPadOrCropd,
+    RandAffined, RandFlipd, RandGaussianNoised, RandAdjustContrastd
+)
 
 class MriDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -24,15 +27,17 @@ class MriDataset(Dataset):
     def __getitem__(self, idx):
         file_name = self.file_names[idx]
 
-        image_np = self.retreive_file(self.path_images, file_name)
-        image_tensor = torch.from_numpy(image_np).float()
-        image_tensor = image_tensor.unsqueeze(0)
-
+        data_dict = {
+            "image": os.path.join(self.path_images, file_name), 
+            "label": os.path.join(self.path_masks, file_name)
+        }
         if self.transform is not None:
-            image_tensor = self.transform(image_tensor)
+            data_dict = self.transform(data_dict)
 
-        mask_np = self.retreive_file(self.path_masks, file_name)
-        mask_tensor = torch.from_numpy(mask_np).long()
+        image_tensor, mask_tensor = data_dict["image"], data_dict["label"]
+
+        # print(type(image_tensor))
+        # raise Exception()
 
         return image_tensor, mask_tensor
 
@@ -41,7 +46,7 @@ class MriDataset(Dataset):
         return np.load(path)
 
 
-def create_transforms(image_size: int) -> A.Compose:
+def create_transforms(image_size: int) -> monai.transforms.Compose:
     """_summary_
 
     Args:
@@ -49,22 +54,28 @@ def create_transforms(image_size: int) -> A.Compose:
         will output a square image, image size must be a multiple of 2^k, where k is the number of U-net pooling steps
 
     Returns:
-        A.Compose: _description_
+        monai.transforms.Compose: _description_
     """
     basic_transform = [
-        A.PadIfNeeded(min_height=image_size, min_width=image_size, border_mode=0, value=0),
-        A.CenterCrop(height=image_size, width=image_size),
-        ToTensorV2(),
+        LoadImaged(keys=["image", "label"]), # Loading py file
+        EnsureChannelFirstd(keys=["image", "label"]), # Adding dimension to be expected format (c, h, w)
+        ResizeWithPadOrCropd(keys=["image", "label"], spatial_size=(image_size, image_size), mode="constant"), # Padding Or cropping to expected size
     ]
 
-    train_transform = A.Compose(
+    train_transform = Compose(
+        basic_transform +
         [
-            
+            RandAffined(keys=["image", "label"], prob=0.3, rotate_range=(0.1, 0.1), scale_range=(0.1, 0.1), mode=("bilinear", "nearest")), # Rotating and Scaling the image
+            # RandFlipd(keys=["image", "label"], prob=0.5, spatial_axis=0), # 
+            RandGaussianNoised(keys=["image"], prob=0.2, std=0.01), # Gaussian noise
+            RandAdjustContrastd(keys=["image"], prob=0.2, gamma=(0.8, 1.2)),
+            EnsureTyped(keys=["image", "label"], dtype=[torch.float32, torch.long])
         ]
-        + basic_transform
     )
 
-    test_transform = A.Compose(basic_transform)
+    test_transform = Compose(
+        basic_transform + [EnsureTyped(keys=["image", "label"], dtype=[torch.float32, torch.long])]
+    )
 
     return train_transform, test_transform
 
